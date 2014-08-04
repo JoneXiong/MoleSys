b04e025fde0455170d612283ecffbe00d6110d19
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2014-08-04 21:11:18 +0800

    删除无关的demo

--- site_apps/crud_example/__init__.py
+++
@@ -1,9 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-
-menus = (
-         ('aboutEmp',u'菜单组一', 'grup_chat'),
-         ('baseinfo',u'菜单组二', 'grup_chart'),
-         ('reportEmp',u'菜单组二', 'grup_disc')
-         )
-
-####### 自定义视图 #########
